f695f816fff508cf7e4a2fd2da8bd34b82edaeba
mark.qj <mark.qj@gmail.com>
2021-10-23 09:37:54 +0800

    Test

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Whitespace: Last Mile on DEV!
+          Whitespace: Good Morning!
         </a>
       </header>
     </div>
